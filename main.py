c610a12a341cdada985f7a312c2dfd3822fede80
Rooner21 <stepka.rus@mail.ru>
2024-04-09 18:30:31 +1000

    second2

--- main.py
+++ main.py
@@ -42,16 +42,3 @@ def register():
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
-
-# from flask import Flask
-
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
-
-# def main():
-#     app.run()
-
-
-# if __name__ == '__main__':
-#     main()
